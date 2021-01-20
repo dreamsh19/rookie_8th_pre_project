806d42c1f8bf42186cf36fbea11dec6bfc614c0b
dreamsh19 <dreamsh19@snu.ac.kr>
2021-01-20 23:51:08 +0900

    Implement and Test guestbookDTO & guestbookService

---
+++ guestbook/src/main/java/org/dreamsh19/guestbook/service/GuestbookService.java
@@ -0,0 +1,18 @@
+package org.dreamsh19.guestbook.service;
+
+import org.dreamsh19.guestbook.dto.GuestbookDTO;
+import org.dreamsh19.guestbook.entity.Guestbook;
+
+public interface GuestbookService {
+    Long register(GuestbookDTO dto);
+
+    default Guestbook dtoToEntity(GuestbookDTO dto){
+        Guestbook entity = Guestbook.builder()
+                .gno(dto.getGno())
+                .title(dto.getTitle())
+                .content(dto.getContent())
+                .writer(dto.getWriter())
+                .build();
+        return entity;
+    }
+}
