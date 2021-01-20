d0a91840ffc8f4f03523db6b293c2f40a2628e41
dreamsh19 <dreamsh19@snu.ac.kr>
2021-01-20 18:08:05 +0900

    Mkae project with dependent libraries

---
+++ guestbook/src/test/java/org/dreamsh19/guestbook/GuestbookApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dreamsh19.guestbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
