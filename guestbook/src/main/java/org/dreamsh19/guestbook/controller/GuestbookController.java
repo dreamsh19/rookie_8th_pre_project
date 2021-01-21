a0ced34c3ba7b546d19724fc715e97598729cc1b
dreamsh19 <dreamsh19@snu.ac.kr>
2021-01-21 12:51:23 +0900

    Implement first page

--- guestbook/src/main/java/org/dreamsh19/guestbook/controller/GuestbookController.java
+++ guestbook/src/main/java/org/dreamsh19/guestbook/controller/GuestbookController.java
@@ -1,19 +1,32 @@
 package org.dreamsh19.guestbook.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.dreamsh19.guestbook.dto.PageRequestDTO;
+import org.dreamsh19.guestbook.service.GuestbookService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/guestbook")
 @Log4j2
+@RequiredArgsConstructor
 public class GuestbookController {
 
-    @GetMapping({"/", "/list"})
-    public String list() {
+    private final GuestbookService service;
+
+    @GetMapping("/")
+    public String index(){
+        return "redirect:/guestbook/list";
+    }
+
+    @GetMapping({"/list"})
+    public void list(PageRequestDTO pageRequestDTO, Model model) {
         log.info("list.....");
 
-        return "/guestbook/list";
+        model.addAttribute("result", service.getList(pageRequestDTO));
+
     }
 }
