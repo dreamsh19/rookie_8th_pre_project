a36bccc74fe7b007a0966a8f5912772e2e9bf96f
dreamsh19 <dreamsh19@snu.ac.kr>
2021-01-21 14:06:17 +0900

    Implement register page with post controller

--- guestbook/src/main/java/org/dreamsh19/guestbook/controller/GuestbookController.java
+++ guestbook/src/main/java/org/dreamsh19/guestbook/controller/GuestbookController.java
@@ -2,12 +2,16 @@ package org.dreamsh19.guestbook.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.dreamsh19.guestbook.dto.GuestbookDTO;
 import org.dreamsh19.guestbook.dto.PageRequestDTO;
+import org.dreamsh19.guestbook.entity.Guestbook;
 import org.dreamsh19.guestbook.service.GuestbookService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/guestbook")
@@ -18,7 +22,7 @@ public class GuestbookController {
     private final GuestbookService service;
 
     @GetMapping("/")
-    public String index(){
+    public String index() {
         return "redirect:/guestbook/list";
     }
 
@@ -28,5 +32,23 @@ public class GuestbookController {
 
         model.addAttribute("result", service.getList(pageRequestDTO));
 
+    }
+
+    @GetMapping("/register")
+    public void register() {
+        log.info("register.....");
+
+    }
+
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes) {
+        log.info("dto... " + dto);
+
+        Long gno = 602L;
+//        Long gno = service.register(dto);
+//
+        redirectAttributes.addFlashAttribute("msg",gno);
+
+        return "redirect:/guestbook/list";
     }
 }
