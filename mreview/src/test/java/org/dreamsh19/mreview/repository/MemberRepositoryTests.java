88f729088491f77729f764c2140d5e973eb20d5b
dreamsh19 <dreamsh19@snu.ac.kr>
2021-01-22 14:53:28 +0900

    Implement and test memberRepository

---
+++ mreview/src/test/java/org/dreamsh19/mreview/repository/MemberRepositoryTests.java
@@ -0,0 +1,27 @@
+package org.dreamsh19.mreview.repository;
+
+import org.dreamsh19.mreview.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class MemberRepositoryTests {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    public void insertMembers() {
+        IntStream.rangeClosed(1, 100).forEach(i -> {
+            Member member = Member.builder()
+                    .email("r" + i + "@test.io")
+                    .password("1234")
+                    .nickname("reviewer " + i)
+                    .build();
+            memberRepository.save(member);
+        });
+    }
+}
