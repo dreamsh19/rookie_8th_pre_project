35b04cec8b5d5423d0fdec75956c5da3e636bf8f
dreamsh19 <dreamsh19@snu.ac.kr>
2021-01-22 14:09:49 +0900

    Implement Member entity

---
+++ mreview/src/main/java/org/dreamsh19/mreview/entity/Member.java
@@ -0,0 +1,27 @@
+package org.dreamsh19.mreview.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+@Table(name = "m_member")
+public class Member extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long mid;
+
+    private String email;
+
+    private String password;
+
+    private String nickname;
+
+
+}
