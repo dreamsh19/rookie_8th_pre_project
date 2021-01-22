6297c796d7aeeb32271111e94b660122ad01976c
dreamsh19 <dreamsh19@snu.ac.kr>
2021-01-22 15:52:55 +0900

    Implement and test for movie list page

--- mreview/src/test/java/org/dreamsh19/mreview/repository/MovieRepositoryTests.java
+++ mreview/src/test/java/org/dreamsh19/mreview/repository/MovieRepositoryTests.java
@@ -5,9 +5,14 @@ import org.dreamsh19.mreview.entity.MovieImage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
 import java.util.UUID;
 import java.util.stream.IntStream;
 
@@ -39,5 +44,16 @@ public class MovieRepositoryTests {
                 movieImageRepository.save(movieImage);
             }
         });
+    }
+
+    @Test
+    public void testListPage() {
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "mno"));
+
+        Page<Object[]> result = movieRepository.getListPage(pageRequest);
+
+        for (Object[] objects : result.getContent()) {
+            System.out.println(Arrays.toString(objects));
+        }
     }
 }
