4516c11367092bb6630e1fcc9a39fe5e40af00d0
dreamsh19 <dreamsh19@snu.ac.kr>
2021-02-11 17:58:00 +0900

    Implement JPQL and tests for list & detail page

--- board/src/main/java/org/dreamsh19/board/repository/BoardRepository.java
+++ board/src/main/java/org/dreamsh19/board/repository/BoardRepository.java
@@ -1,6 +1,8 @@
 package org.dreamsh19.board.repository;
 
 import org.dreamsh19.board.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -18,4 +20,22 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     // FK 없음. on 필요
     @Query("select b,r from Board b left join Reply r on r.board = b where b.bno=:bno")
     List<Object[]> getBoardWithReply(@Param("bno") Long bno);
+
+    // 목록 가져오기 (목록 화면)
+    @Query("select b, w, count(r) " +
+            "from Board b " +
+            "left join b.writer w " +
+            "left join Reply r on r.board = b " +
+            "group by b")
+    Page<Object[]> getBoardListWithReplyCount(Pageable pageable);
+
+    // 특정 bno 가져오기 (조회 화면)
+    @Query("select b, w, count(r) " +
+            "from Board b " +
+            "left join b.writer w " +
+            "left join Reply r on r.board =b " +
+            "where b.bno = :bno ")
+    Object getBoardByBnoWithReplyCount(@Param("bno") Long bno);
+    
 }
+
