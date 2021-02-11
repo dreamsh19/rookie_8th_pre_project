7fd2a166f82639d5420764f84b32ae1b4c7fdfeb
dreamsh19 <dreamsh19@snu.ac.kr>
2021-02-12 00:36:02 +0900

    Implement and test service & DTO for list

--- board/src/main/java/org/dreamsh19/board/service/BoardServiceImpl.java
+++ board/src/main/java/org/dreamsh19/board/service/BoardServiceImpl.java
@@ -2,13 +2,21 @@ package org.dreamsh19.board.service;
 
 import lombok.RequiredArgsConstructor;
 import org.dreamsh19.board.dto.BoardDTO;
+import org.dreamsh19.board.dto.PageRequestDTO;
+import org.dreamsh19.board.dto.PageResultDTO;
 import org.dreamsh19.board.entity.Board;
+import org.dreamsh19.board.entity.Member;
 import org.dreamsh19.board.repository.BoardRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
 
 @Service
 @RequiredArgsConstructor
-public class BoardServiceImpl implements BoardService{
+public class BoardServiceImpl implements BoardService {
 
     private final BoardRepository boardRepository;
 
@@ -20,4 +28,16 @@ public class BoardServiceImpl implements BoardService{
         // 이때 board.bno != null
         return board.getBno();
     }
+
+    @Override
+    public PageResultDTO<Object[], BoardDTO> getList(PageRequestDTO pageRequestDTO) {
+        Pageable pageable = pageRequestDTO.getPageable(Sort.by("bno").descending());
+        Page<Object[]> result = boardRepository.getBoardListWithReplyCount(pageable);
+        Function<Object[], BoardDTO> fn = (row ->
+                entityToDTO((Board) row[0], (Member) row[1], (Long) row[2]));
+
+        return new PageResultDTO<>(result, fn);
+    }
+
+
 }
--- board/src/test/java/org/dreamsh19/board/service/BoardServiceTests.java
+++ board/src/test/java/org/dreamsh19/board/service/BoardServiceTests.java
@@ -1,6 +1,8 @@
 package org.dreamsh19.board.service;
 
 import org.dreamsh19.board.dto.BoardDTO;
+import org.dreamsh19.board.dto.PageRequestDTO;
+import org.dreamsh19.board.dto.PageResultDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,12 +14,20 @@ public class BoardServiceTests {
     private BoardService boardService;
 
     @Test
-    public void registerTest(){
+    public void registerTest() {
         BoardDTO dto = BoardDTO.builder()
                 .title("test title")
                 .content("test content")
                 .writerEmail("user1@test.io")
                 .build();
         Long bno = boardService.register(dto);
+    }
+
+    @Test
+    public void listTest() {
+        PageResultDTO<Object[], BoardDTO> pageResultDTO = boardService.getList(new PageRequestDTO());
+        for (BoardDTO boardDTO : pageResultDTO.getDtoList()) {
+            System.out.println(boardDTO);
+        }
     }
 }
