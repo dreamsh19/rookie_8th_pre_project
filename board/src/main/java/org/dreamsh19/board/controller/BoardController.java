c1f942ac802a20a91fd48fb8d1639876724011b0
dreamsh19 <dreamsh19@snu.ac.kr>
2021-02-14 00:07:05 +0900

    Implement register page

--- board/src/main/java/org/dreamsh19/board/controller/BoardController.java
+++ board/src/main/java/org/dreamsh19/board/controller/BoardController.java
@@ -1,13 +1,16 @@
 package org.dreamsh19.board.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.dreamsh19.board.dto.BoardDTO;
 import org.dreamsh19.board.dto.PageRequestDTO;
 import org.dreamsh19.board.dto.PageResultDTO;
 import org.dreamsh19.board.service.BoardService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/board")
@@ -21,5 +24,19 @@ public class BoardController {
     public void list(PageRequestDTO pageRequestDTO, Model model){
 
         model.addAttribute("pageResult", boardService.getList(pageRequestDTO));
+    }
+
+    @GetMapping("/register")
+    public void register(){
+
+    }
+
+    @PostMapping("/register")
+    public String registerPost(BoardDTO dto, RedirectAttributes redirectAttributes){
+        Long bno = boardService.register(dto);
+
+        redirectAttributes.addFlashAttribute("msg", bno);
+
+        return "redirect:/board/list";
     }
 }
